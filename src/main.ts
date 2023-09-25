4471ddfd8a40d1f2afe50e50576ac9369ab9d29c
Anderson Viana <anderson11viana@gmail.com>
2023-09-25 10:29:36 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  var whitelist = ['https://ordem-de-servicos.vercel.app'];
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,UPDATE',
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
   await app.listen(3000);
