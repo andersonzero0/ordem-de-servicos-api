88a69ee8fd105344d7e6be2537783e1c78f56645
Anderson Viana <anderson11viana@gmail.com>
2023-09-25 11:23:25 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://ordem-de-servicos.vercel.app'],
-    methods: ['GET','PUT','POST'],
+    methods: ['GET','PUT','POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
   await app.listen(3000);
