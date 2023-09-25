7f28837518087f4ffaa6bafaa08d4d55d3edc3c9
Anderson Viana <anderson11viana@gmail.com>
2023-09-25 10:56:21 -0300

    methods

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET,PUT,POST',
+    methods: ['GET','PUT','POST'],
     credentials: true,
   });
   await app.listen(3000);
