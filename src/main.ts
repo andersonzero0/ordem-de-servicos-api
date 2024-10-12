2831d2894476869c02740d9dd7b35537226e91bb
Anderson Viana <programadorzero@hotmail.com>
2024-10-12 19:01:24 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://ordem-de-servicos.vercel.app'],
+    origin: "*",
     methods: ['GET','PUT','POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
